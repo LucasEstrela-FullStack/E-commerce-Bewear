3a39cf74a2a7af7d1a62ddca0352c012d3840a21
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-08-09 03:02:15 -0400

    feat: add cart item

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -6,6 +6,7 @@ import { ShoppingBasketIcon } from "lucide-react";
 import Image from "next/image";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "@/actions/get-cart/index";
+import CartItem from "./cart-item";
 
 export const Cart = () => {
  const { data: cart, isPending: cartIsLoading } = useQuery({
@@ -23,20 +24,18 @@ export const Cart = () => {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div>
+        <div  className="space-y-4 px-5">
           {cartIsLoading && <div>Carregando...</div>}
           {cart?.items.map((item) => (
-            <div key={item.id}>
-              <Image
-                src={item.productVariant.imageUrl}
-                alt={item.productVariant.product.name}
-                width={100}
-                height={100}
-              />
-              <div>
-                <h3>{item.productVariant.product.name}</h3>
-              </div>
-            </div>
+            <CartItem
+              key={item.id}
+              id={item.id}
+              productName={item.productVariant.product.name}
+              productVariantName={item.productVariant.name}
+              productVariantImageUrl={item.productVariant.imageUrl}
+              productVariantPriceInCents={item.productVariant.priceInCents}
+              quantity={item.quantity}
+            />
           ))}
         </div>
       </SheetContent>

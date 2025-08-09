c6fecd98bffa2dd04de9bd5d4d8156dc41889d3a
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-08-09 02:32:34 -0400

    feat: ensure a product can be added to cart and add cart component

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,20 +1,47 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 import { ShoppingBasketIcon } from "lucide-react";
+import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import { getCart } from "@/actions/get-cart/index";
 
 export const Cart = () => {
- return <Sheet>
-    <SheetTrigger asChild>
+ const { data: cart, isPending: cartIsLoading } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () => getCart(),
+});
+ return (
+   <Sheet>
+      <SheetTrigger asChild>
         <Button variant="outline" size="icon">
-           <ShoppingBasketIcon />
+          <ShoppingBasketIcon />
         </Button>
-    </SheetTrigger>
-    <SheetContent>
-
-    </SheetContent>
- </Sheet>
-}
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Carrinho</SheetTitle>
+        </SheetHeader>
+        <div>
+          {cartIsLoading && <div>Carregando...</div>}
+          {cart?.items.map((item) => (
+            <div key={item.id}>
+              <Image
+                src={item.productVariant.imageUrl}
+                alt={item.productVariant.product.name}
+                width={100}
+                height={100}
+              />
+              <div>
+                <h3>{item.productVariant.product.name}</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </SheetContent>
+    </Sheet>
+ );
+};
  
 // Server Action
